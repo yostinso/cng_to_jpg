b4d20c5054536b2e90017e15d68728df6a4cc514
E.O. Stinson <yostinso@aradine.com>
2024-08-17 22:30:15 -0700

    Add zip download support
    Since "download all" seems to max out at 10 files.

--- cng_to_jpg.js
+++ cng_to_jpg.js
@@ -42,6 +42,8 @@ function processFiles(fileList) {
 
 function layoutImages(imgs) {
     var jpegs = document.getElementById('jpegs');
+    document.querySelectorAll('#jpegs img').forEach((img) => URL.revokeObjectURL(img.src));
+    jpegs.innerHTML = '';
     imgs.forEach((img, i) => {
         var container = document.createElement('div');
         jpegs.appendChild(container);
@@ -49,9 +51,17 @@ function layoutImages(imgs) {
     });
     if (imgs.length > 0) {
         var controls = document.getElementById('controls');
-        var downloadButton = document.querySelector('#controls input');
+        var downloadButton = document.getElementById('downloadAll');
         downloadButton.value = 'Download All ' + imgs.length + ' image(s)';
         controls.style.display = 'flex';
+    }
+
+    var downloadZipButton = document.getElementById('downloadZip');
+    var hasZip = window.hasOwnProperty('zip');
+    if (imgs.length > 1 && hasZip) {
+        downloadZipButton.style.display = 'block';
+    } else {
+        downloadZipButton.style.display = 'none';
     }
 }
 
@@ -104,4 +114,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
             a.click();
         });
     });
+
+    var hasZip = window.hasOwnProperty('zip');
+    if (hasZip) {
+        var downloadZip = document.getElementById('downloadZip');
+        downloadZip.addEventListener('click', function() {
+            var imgs = document.querySelectorAll('#jpegs img');
+            var blobWriter = new zip.BlobWriter("application/zip");
+            var zipWriter = new zip.ZipWriter(blobWriter);
+            var imgs = document.querySelectorAll('#jpegs img');
+
+            Promise.all(
+                Array.from(imgs).map((img) => {
+                    let filename = img.alt.replace(/.cng/i, '.jpg');
+                    return fetch(img.src).then((r) => r.blob()).then((blob) => {
+                        zipWriter.add(filename, blob.stream(), { level: 0 });
+                    });
+                })
+            ).then(() => {
+                zipWriter.close();
+            }).then(() => {
+                blobWriter.getData().then((blob) => {
+                    var a = document.createElement('a');
+                    a.href = URL.createObjectURL(blob);
+                    a.download = 'images.zip';
+                    a.click();
+                    URL.revokeObjectURL(a.href);
+                });
+            });
+        });
+    }
 });
