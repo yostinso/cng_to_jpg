012cbf0781b3588855cb56bbb2357ff18bb0d9df
E.O. Stinson <yostinso@aradine.com>
2024-08-20 19:33:44 -0700

    Support recursive folder conversion

--- cng_to_jpg.js
+++ cng_to_jpg.js
@@ -1,42 +1,69 @@
-function loadFile(file) {
-    return new Promise((resolve, reject) => {
-        let fileReader = new FileReader();
-        fileReader.addEventListener('loadend', (event) => {
-            resolve({
-                name: file.name,
-                type: file.type,
-                size: file.size,
-                data: new Uint8Array(fileReader.result)
+class LoadedFile {
+    static async create(file) {
+        let promise = new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+            fileReader.addEventListener('loadend', (event) => {
+                resolve({
+                    name: file.name,
+                    size: file.size,
+                    data: new Uint8Array(fileReader.result)
+                });
             });
+            fileReader.addEventListener('error', (event) => {
+                reject(event);
+            });
+            fileReader.addEventListener('abort', (event) => {
+                reject(event);
+            });
+            fileReader.readAsArrayBuffer(file);
         });
-        fileReader.addEventListener('error', (event) => {
-            reject(event);
-        });
-        fileReader.addEventListener('abort', (event) => {
-            reject(event);
-        });
-        fileReader.readAsArrayBuffer(file);
-    });
-}
-function xorFile(loadedFile) {
-    return {
-        ...loadedFile,
-        data: loadedFile.data.map((byte) => byte ^ 0xef)
+        let data = await promise;
+        return new LoadedFile(data);
+    }
+    constructor({ name, size, data }) {
+        this.name = name;
+        this.size = size;
+        this.data = data;
+    }
+    extname() {
+        return this.name.split('.').pop().toLowerCase();
+    }
+    type() {
+        return this.extname() === 'cng' ? 'application/octet-stream' : 'image/jpeg';
+    }
+    toJpg() {
+        if (this.extname() === 'cng') {
+            this.xor();
+            this.name = this.name.replace(/.cng/i, '.jpg');
+        }
+        return this;
+    }
+    toCng() {
+        if (this.extname() === 'jpg') {
+            this.xor();
+            this.name = this.name.replace(/.jpg/i, '.cng');
+        }
+        return this;
+    }
+    xor() {
+        this.data = this.data.map((byte) => byte ^ 0xef);
     }
 }
-function processFiles(fileList) {
-    return Array.from(fileList).map((file) => {
-        var xoredFiles = loadFile(file).then((file) => xorFile(file));
-        return xoredFiles.then((xoredFile) => {
-            var jpeg = new Blob([xoredFile.data], {type: xoredFile.type});
-            var jpegUrl = URL.createObjectURL(jpeg);
-            var img = document.createElement('img');
-            img.alt = xoredFile.name;
-            img.src = jpegUrl;
-            return img;
-        }).catch((error) => {
-            console.error(error);
-        });
+
+async function processFiles(fileList) {
+    let loadedFiles = [];
+    for (let file of fileList) {
+        let loadedFile = await LoadedFile.create(file);
+        loadedFile.toJpg();
+        loadedFiles.push(loadedFile);
+    }
+    return loadedFiles.map((loadedFile) => {
+        let jpeg = new Blob([loadedFile.data], { type: loadedFile.type() });
+        let jpegUrl = URL.createObjectURL(jpeg);
+        let img = document.createElement('img');
+        img.alt = loadedFile.name;
+        img.src = jpegUrl;
+        return img;
     });
 }
 
@@ -49,11 +76,6 @@ function layoutImages(imgs) {
         jpegs.appendChild(container);
         container.appendChild(img);
     });
-    if (imgs.length > 0) {
-        var controls = document.getElementById('controls');
-        controls.style.display = 'flex';
-    }
-
     var downloadButton = document.getElementById('downloadAll');
     if (imgs.length > 0 && imgs.length <= 10) {
         downloadButton.style.display = 'block';
@@ -72,6 +94,48 @@ function layoutImages(imgs) {
         downloadZipButton.style.display = 'block';
     } else {
         downloadZipButton.style.display = 'none';
+    }
+}
+
+async function readDirectory(folderPromise, path = '') {
+    var cngFiles = [];
+    const folderHandle = await folderPromise;
+    path = path + folderHandle.name + '/';
+    for await (let entry of folderHandle.values()) {
+        if (entry.kind === 'file' && entry.name.toLowerCase().endsWith('.cng')) {
+            file = await LoadedFile.create(await entry.getFile());
+            file.path = path;
+            cngFiles.push(file);
+        } else if (entry.kind === 'directory') {
+            cngFiles = [ ...cngFiles, ...await readDirectory(entry, path)];
+        }
+    }
+    return cngFiles;
+}
+
+async function convertAndZipDirectory(folderPromise) {
+    let files = await readDirectory(folderPromise);
+    var hasZip = window.hasOwnProperty('zip');
+    if (hasZip) {
+        var blobWriter = new zip.BlobWriter("application/zip");
+        var zipWriter = new zip.ZipWriter(blobWriter);
+        for (let i in files) {
+            let file = files[i];
+            file.toJpg();
+            await zipWriter.add(
+                file.path + file.name,
+                new zip.Uint8ArrayReader(file.data),
+                { level: 0 }
+            )
+        }
+        await zipWriter.close();
+        await blobWriter.getData().then((blob) => {
+            var a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = 'images.zip';
+            a.click();
+            URL.revokeObjectURL(a.href);
+        });
     }
 }
 
@@ -95,7 +159,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         e.stopPropagation();
         dropzone.classList.remove('dragover');
         var files = e.dataTransfer.files;
-        Promise.all(processFiles(files)).then((imgs) => {
+        processFiles(files).then((imgs) => {
             layoutImages(imgs);
         });
     });
@@ -107,12 +171,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
         input.accept = '.cng';
         input.addEventListener('change', function(e) {
             var files = e.target.files;
-            Promise.all(processFiles(files)).then((imgs) => {
+            processFiles(files).then((imgs) => {
                 layoutImages(imgs);
             });
         });
         input.click();
     });
+
+    var hasZip = window.hasOwnProperty('zip');
+    if (hasZip) {
+        var chooseFolderButton = document.getElementById('chooseFolder');
+        chooseFolderButton.addEventListener('click', function() {
+            convertAndZipDirectory(
+                window.showDirectoryPicker({ id: "cngs", startIn: "downloads" })
+            )
+        });
+    } else {
+        chooseFolderButton.style.display = 'none';
+    }
 
     var downloadAll = document.getElementById('downloadAll');
     downloadAll.addEventListener('click', function() {
@@ -125,7 +201,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    var hasZip = window.hasOwnProperty('zip');
     if (hasZip) {
         var downloadZip = document.getElementById('downloadZip');
         downloadZip.addEventListener('click', function() {
